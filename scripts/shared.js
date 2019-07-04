eb262fed90629240b18b3ed6320103a2d7c0f642
Brian Furches <dbfurches@gmail.com>
2019-07-04 16:59:45 -0400

    got everything working

--- scripts/shared.js
+++ scripts/shared.js
@@ -1,7 +1,11 @@
+
+
 $.ajaxSetup({  
+
   
-    xhrFields: {
-        origin: 'http://127.0.0.1:8080',
+  
+    xhrFields: {      
+        origin: getAPIOrigin(),
         withCredentials: true
     }
 })
@@ -10,12 +14,20 @@ const API_URL = getHostURL();
 const AUTH_URL = `${API_URL}/auth`
 
 function getHostURL() {
-    // if (window.location.host.indexOf('localhost') != -1) {
-      return 'http://127.0.0.1:3000';
-    // } else {
-    //   return 'https://sticker-mania.herokuapp.com';
-    // }
+    if (window.location.host.indexOf('127.0.0.1') != -1) {
+      return 'http://127.0.0.1:3001';
+    } else {
+      return 'https://api.gobmark.com';
+    }
   };
+
+function getAPIOrigin() {
+  if (window.location.host.indexOf('127.0.0.1') != -1) {
+    return 'http://127.0.0.1:8080'
+  } else {
+    return 'https://www.gobmark.com'
+  }
+}
 
 function getUserFromForm () {
   const email = $('#email').val();
